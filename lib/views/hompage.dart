d316616a52326dbcb244f44792bb5367eccaa04e
mahi meena <mahi0507>
2021-07-26 01:37:45 +0530

    added more screens

--- lib/views/hompage.dart
+++ lib/views/hompage.dart
@@ -1,17 +1,142 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:quizapp/services/database.dart';
+import 'package:quizapp/views/create_quiz.dart';
+import 'package:quizapp/views/quiz_play.dart';
+import 'package:quizapp/widget/widget.dart';
 
 class HomePage extends StatefulWidget {
-
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  Stream quizStream;
+  DatabaseService databaseService = new DatabaseService();
+
+  Widget quizList() {
+    return Container(
+      child: Column(
+        children: [
+          StreamBuilder(
+            stream: quizStream,
+            builder: (context, snapshot) {
+              return snapshot.data == null
+                  ? Container()
+                  : ListView.builder(
+                  shrinkWrap: true,
+                  physics: ClampingScrollPhysics(),
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (context, index) {
+                    return QuizTile(
+                      noOfQuestions: snapshot.data.documents.length,
+                      imageUrl:
+                      snapshot.data.documents[index].data['quizImgUrl'],
+                      title:
+                      snapshot.data.documents[index].data['quizTitle'],
+                      description:
+                      snapshot.data.documents[index].data['quizDesc'],
+                      id: snapshot.data.documents[index].data["id"],
+                    );
+                  });
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    databaseService.getQuizData().then((value) {
+      quizStream = value;
+      setState(() {});
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: AppLogo(),
+        brightness: Brightness.light,
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        //brightness: Brightness.li,
+      ),
+      body: quizList(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => CreateQuiz()));
+        },
+      ),
+    );
+  }
+}
+
+class QuizTile extends StatelessWidget {
+  final String imageUrl, title, id, description;
+  final int noOfQuestions;
+
+  QuizTile(
+      {@required this.title,
+        @required this.imageUrl,
+        @required this.description,
+        @required this.id,
+        @required this.noOfQuestions});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(
+            builder: (context) => QuizPlay(id)
+        ));
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        height: 150,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Stack(
+            children: [
+              Image.network(
+                imageUrl,
+                fit: BoxFit.cover,
+                width: MediaQuery.of(context).size.width,
+              ),
+              Container(
+                color: Colors.black26,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        title,
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(height: 4,),
+                      Text(
+                        description,
+                        style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
